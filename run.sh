605325e1a60de9d26d7b1a06a8eb84d43332510e
Tuan <tuanphung2308@gmail.com>
2021-08-23 15:24:56 +0700

    List all files in path

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/springboot-hello
+# List all file first
+echo "Files in directory"
+pwd; ls -l
+echo "--------------------------------------------------"
 docker-compose build --no-cache
 docker-compose up -d
