481d577a0946ad26b49571cecdfbe655a32ee1c3
Tuan <tuanphung2308@gmail.com>
2021-08-23 15:27:39 +0700

    Double checking the path

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd /home/ec2-user/
+echo "Files in user directory"
+pwd; ls -l
+echo "--------------------------------------------------"
 cd /home/ec2-user/springboot-hello
 # List all file first
-echo "Files in directory"
+echo "Files in source directory"
 pwd; ls -l
 echo "--------------------------------------------------"
 docker-compose build --no-cache
